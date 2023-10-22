38d0e03e31b33de768a9951a65679efcffcbea23
Mika Hiltunen <github@haaska.net>
2023-10-22 10:04:31 +0300

    Websocket connection checked and event queue

--- ui/static/websocket.js
+++ ui/static/websocket.js
@@ -2,42 +2,68 @@
 import { setItems, setFavorites } from "./app.js";
 
 let socket;
+let eventQueue = [];
 
 export const initializeWebSocket = () => {
     let protocol = window.location.protocol === "https:" ? "wss://" : "ws://";
     socket = new WebSocket(protocol + window.location.host + "/ws")
 
     socket.addEventListener("open", (event) => {
-        socket.send(JSON.stringify({"type": "INITIAL"}));
+        console.debug("Websocket opened")
+        if (eventQueue.length === 0) {
+            socket.send(JSON.stringify({"type": "INITIAL"}));
+        } else {
+            while (eventQueue.length > 0) {
+                sendEvent(eventQueue.pop());
+            }
+        }
     });
-    
+
     socket.addEventListener("message", (event) => {
         handleWsMessage(event.data);
+    });
+
+    socket.addEventListener("close", (event) => {
+        console.debug("Websocket closed");
+    });
+
+    socket.addEventListener("error", (event) => {
+        console.error("Websocket error:", event);
     });
 };
 
 export const sendAddItemEvent = (itemName) => {
-    socket.send(JSON.stringify({"type": "ADD_ITEM", "name": itemName}));
+    sendEvent({"type": "ADD_ITEM", "name": itemName});
 }
 
 export const sendSetItemStatusEvent = (itemId, checked) => {
-    socket.send(JSON.stringify({"type": "SET_ITEM_STATUS", "id": itemId, checked}));
+    sendEvent({"type": "SET_ITEM_STATUS", "id": itemId, checked});
 }
 
 export const sendRemoveItemEvent = (itemId) => {
-    socket.send(JSON.stringify({"type": "REMOVE_ITEM", "id": itemId}));
+    sendEvent({"type": "REMOVE_ITEM", "id": itemId});
 }
 
 export const sendSortItemsEvent = (sortedItems) => {
-    socket.send(JSON.stringify({ "type": "SORT_ITEMS", "items": sortedItems }));
+    sendEvent({ "type": "SORT_ITEMS", "items": sortedItems });
 }
 
 export const sendRemoveFavoriteEvent = (itemName) => {
-    socket.send(JSON.stringify({ "type": "REMOVE_FAVORITE", "name": itemName }));
+    sendEvent({ "type": "REMOVE_FAVORITE", "name": itemName });
+}
+
+const sendEvent = (eventObject) => {
+    if (socket.readyState === WebSocket.OPEN) {
+        socket.send(JSON.stringify(eventObject));
+    } else {
+        eventQueue.push(eventObject);
+        initializeWebSocket();
+    }
 }
 
 const handleWsMessage = (wsMessage) => {
     const message = JSON.parse(wsMessage)
+    console.debug("Websocket message received:", message.type);
     if (message.type == "LIST_UPDATED") {
         const items = message.body.items;
         setItems(items);
